0688e4f9ea4881d150b312b35a5b1a2ba244ba2e
liuyibing123456 <liuyibing123456@126.com>
2024-06-23 22:01:59 +0800

    init

---
+++ alatka-connection-test/src/main/java/com/alatka/connection/test/AutoConfiguration.java
@@ -0,0 +1,13 @@
+package com.alatka.connection.test;
+
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ybliu
+ */
+@Configuration
+@ImportAutoConfiguration({})
+public class AutoConfiguration {
+
+}
