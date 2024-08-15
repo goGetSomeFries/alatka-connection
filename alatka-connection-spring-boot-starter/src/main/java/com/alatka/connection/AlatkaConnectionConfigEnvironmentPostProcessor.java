e3356cc904e6fddf3721c03e58221daef5de8190
mocker <liuyibing123456@126.com>
2024-08-15 18:15:39 +0800

    init

--- alatka-connection-spring-boot-starter/src/main/java/com/alatka/connection/AlatkaConnectionConfigEnvironmentPostProcessor.java
+++ alatka-connection-spring-boot-starter/src/main/java/com/alatka/connection/AlatkaConnectionConfigEnvironmentPostProcessor.java
@@ -11,9 +11,10 @@ import org.springframework.core.io.Resource;
 import java.io.IOException;
 
 /**
- * 构造connection默认变量<br>
+ * 构造alatka-connection默认变量<br>
+ * alatka-connection-core alatka-connection-http alatka-connection-socket等模块中如使用变量可在该文件中进行配置<br>
  * 文件路径：{@link AlatkaConnectionConfigEnvironmentPostProcessor#CONNECTION_CONFIG_YML}<br>
- * 参数优先级：application.yml > alatka-connection-config.yaml
+ * 参数优先级：application.yml > alatka-connection-config.yaml，可通过配置application.yml中进行覆盖<br>
  *
  * @author ybliu
  */
