9092d9e2ebd0e75052c9ae2198dc189ff5eaf1c3
mocker <liuyibing123456@126.com>
2024-08-15 16:05:15 +0800

    init

--- alatka-connection-spring-boot-starter/src/main/java/com/alatka/connection/AlatkaConnectionConfigEnvironmentPostProcessor.java
+++ alatka-connection-spring-boot-starter/src/main/java/com/alatka/connection/AlatkaConnectionConfigEnvironmentPostProcessor.java
@@ -5,7 +5,6 @@ import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.boot.env.YamlPropertySourceLoader;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
@@ -13,14 +12,14 @@ import java.io.IOException;
 
 /**
  * 构造connection默认变量<br>
- * 文件路径：{@link ConnectionConfigEnvironmentPostProcessor#CONNECTION_CONFIG_YML}<br>
- * 参数优先级：application.yml > connection-config.yml
+ * 文件路径：{@link AlatkaConnectionConfigEnvironmentPostProcessor#CONNECTION_CONFIG_YML}<br>
+ * 参数优先级：application.yml > alatka-connection-config.yaml
  *
  * @author ybliu
  */
-public class ConnectionConfigEnvironmentPostProcessor implements EnvironmentPostProcessor, Ordered {
+public class AlatkaConnectionConfigEnvironmentPostProcessor implements EnvironmentPostProcessor, Ordered {
 
-    private static final String CONNECTION_CONFIG_YML = "META-INF/connection-config.yml";
+    private static final String CONNECTION_CONFIG_YML = "META-INF/alatka-connection-config.yaml";
 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
@@ -28,8 +27,8 @@ public class ConnectionConfigEnvironmentPostProcessor implements EnvironmentPost
         if (resource.exists()) {
             YamlPropertySourceLoader loader = new YamlPropertySourceLoader();
             try {
-                PropertySource<?> propertySource = loader.load(CONNECTION_CONFIG_YML, resource).get(0);
-                environment.getPropertySources().addLast(propertySource);
+                loader.load(CONNECTION_CONFIG_YML, resource)
+                        .forEach(propertySource -> environment.getPropertySources().addLast(propertySource));
             } catch (IOException e) {
                 throw new IllegalStateException("Failed to load gateway config properties from " + resource, e);
             }
