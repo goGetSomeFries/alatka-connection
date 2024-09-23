90f0c3fda23d16051f1f101a5f0d99080aafdc96
mocker <liuyibing123456@126.com>
2024-09-23 17:49:06 +0800

    init

--- alatka-connection-ratelimiter/src/main/java/com/alatka/connection/ratelimiter/component/handler/RateLimiterHandlerRegister.java
+++ alatka-connection-ratelimiter/src/main/java/com/alatka/connection/ratelimiter/component/handler/RateLimiterHandlerRegister.java
@@ -2,9 +2,11 @@ package com.alatka.connection.ratelimiter.component.handler;
 
 import com.alatka.connection.core.component.handler.HandlerComponentRegister;
 import com.alatka.connection.core.property.core.HandlerProperty;
+import com.alatka.connection.ratelimiter.support.RateLimiterHandler;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
-import org.springframework.integration.handler.MessageProcessor;
 import org.springframework.integration.handler.ServiceActivatingHandler;
+
+import java.util.Map;
 
 /**
  * TODO
@@ -13,10 +15,24 @@ import org.springframework.integration.handler.ServiceActivatingHandler;
  */
 public class RateLimiterHandlerRegister extends HandlerComponentRegister<HandlerProperty> {
 
+    private static final String KEY_PERMITS_PER_SECOND = "permitsPerSecond";
+    private static final String KEY_WARMUP_PERIOD = "warmupPeriod";
+    private static final String KEY_BLOCKED = "blocked";
+    private static final String KEY_TIMEOUT = "timeout";
+
     @Override
     protected void doRegister(BeanDefinitionBuilder builder, HandlerProperty property) {
-        MessageProcessor<Object> messageProcessor = message -> null;
-        builder.addConstructorArgValue(messageProcessor);
+        Map<String, Object> params = property.getParams();
+        Integer permitsPerSecond = (Integer) params.get(KEY_PERMITS_PER_SECOND);
+        Integer warmupPeriod = (Integer) params.get(KEY_WARMUP_PERIOD);
+        boolean blocked = (boolean) params.getOrDefault(KEY_BLOCKED, false);
+        Integer timeout = (Integer) params.getOrDefault(KEY_TIMEOUT, 0);
+
+        RateLimiterHandler handler = warmupPeriod == null ?
+                new RateLimiterHandler(permitsPerSecond) : new RateLimiterHandler(permitsPerSecond, warmupPeriod);
+        handler.setBlocked(blocked);
+        handler.setTimeout(timeout);
+        builder.addConstructorArgValue(handler);
     }
 
     @Override
--- alatka-connection-ratelimiter/src/main/java/com/alatka/connection/ratelimiter/support/RateLimiterHandler.java
+++ alatka-connection-ratelimiter/src/main/java/com/alatka/connection/ratelimiter/support/RateLimiterHandler.java
@@ -1,12 +1,48 @@
 package com.alatka.connection.ratelimiter.support;
 
+import com.google.common.util.concurrent.RateLimiter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.integration.handler.MessageProcessor;
 import org.springframework.messaging.Message;
 
+import java.util.concurrent.TimeUnit;
+
 public class RateLimiterHandler implements MessageProcessor<Object> {
+
+    private final Logger logger = LoggerFactory.getLogger(RateLimiterHandler.class);
+
+    private RateLimiter rateLimiter;
+
+    private boolean blocked;
+
+    private Integer timeout;
+
+    public RateLimiterHandler(Integer permitsPerSecond) {
+        this.rateLimiter = RateLimiter.create(permitsPerSecond.doubleValue());
+    }
+
+    public RateLimiterHandler(Integer permitsPerSecond, Integer warmupPeriod) {
+        this.rateLimiter = RateLimiter.create(permitsPerSecond, warmupPeriod.longValue(), TimeUnit.SECONDS);
+    }
 
     @Override
     public Object processMessage(Message<?> message) {
-        return null;
+        if (blocked) {
+            double acquire = this.rateLimiter.acquire();
+            this.logger.debug("获取令牌成功，等待时间{}s", acquire);
+            return message;
+        }
+        boolean flag = this.rateLimiter.tryAcquire(this.timeout.longValue(), TimeUnit.SECONDS);
+        this.logger.debug("获取令牌{}，等待时间{}s", flag ? "成功" : "失败", this.timeout);
+        return flag ? message : null;
+    }
+
+    public void setBlocked(boolean blocked) {
+        this.blocked = blocked;
+    }
+
+    public void setTimeout(Integer timeout) {
+        this.timeout = timeout;
     }
 }
