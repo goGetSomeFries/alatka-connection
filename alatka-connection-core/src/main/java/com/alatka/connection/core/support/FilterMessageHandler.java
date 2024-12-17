a669eb6c75532e8711e30cc55f3953a1465119aa
asuka <liuyibing@163.com>
2024-12-17 16:20:24 +0800

    filter组件优化

--- alatka-connection-core/src/main/java/com/alatka/connection/core/support/FilterMessageHandler.java
+++ alatka-connection-core/src/main/java/com/alatka/connection/core/support/FilterMessageHandler.java
@@ -1,11 +1,20 @@
 package com.alatka.connection.core.support;
 
+import org.springframework.messaging.Message;
+
 /**
  * @param <T> 方法入参类型
  * @author whocares
  * @see com.alatka.connection.core.component.handler.FilterHandlerRegister
  */
 public interface FilterMessageHandler<T> extends CustomMessageHandler<T, Boolean> {
+
+    boolean test(Message<T> message);
+
+    @Override
+    default Boolean doExecute(Message<T> message) {
+        return this.test(message);
+    }
 
     @Override
     default boolean returnRawType() {
