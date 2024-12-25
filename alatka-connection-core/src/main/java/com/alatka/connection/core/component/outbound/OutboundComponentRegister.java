e1a30202dee772713d52be3e157e3b8eee581cc0
mocker <liuyibing123456@126.com>
2024-12-25 18:17:51 +0800

    Outbound单工组件outputChannel问题修复

--- alatka-connection-core/src/main/java/com/alatka/connection/core/component/outbound/OutboundComponentRegister.java
+++ alatka-connection-core/src/main/java/com/alatka/connection/core/component/outbound/OutboundComponentRegister.java
@@ -2,6 +2,7 @@ package com.alatka.connection.core.component.outbound;
 
 import com.alatka.connection.core.component.handler.HandlerComponentRegister;
 import com.alatka.connection.core.property.core.OutboundProperty;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 
 /**
  * TODO
@@ -10,4 +11,13 @@ import com.alatka.connection.core.property.core.OutboundProperty;
  */
 public abstract class OutboundComponentRegister<T extends OutboundProperty> extends HandlerComponentRegister<T> {
 
+    @Override
+    protected void preDoRegister(BeanDefinitionBuilder builder, T property) {
+        if (property.getOutputChannel() != null) {
+            builder.addPropertyValue("outputChannelName", property.getOutputChannel());
+        }
+        if (property.getOrder() != null) {
+            builder.addPropertyValue("order", property.getOrder());
+        }
+    }
 }
