f03bdbb5eb413336c672e31464e36b85d329c56d
mocker <liuyibing123456@126.com>
2025-02-17 18:36:04 +0800

    inbound模块增加gateway组件

---
+++ alatka-connection-core/src/main/java/com/alatka/connection/core/property/core/GatewayInboundProperty.java
@@ -0,0 +1,10 @@
+package com.alatka.connection.core.property.core;
+
+/**
+ * alatka.connection.route.inbound.gateway
+ *
+ * @author ybliu
+ */
+public class GatewayInboundProperty extends InboundProperty {
+
+}
