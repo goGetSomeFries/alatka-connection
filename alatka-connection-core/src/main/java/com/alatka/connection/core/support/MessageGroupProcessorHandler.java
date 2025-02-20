4bcb04d44fd2193d7d9abe403657270b503774b3
liuyibing123456 <liuyibing123456@126.com>
2025-02-20 23:01:00 +0800

    init

--- alatka-connection-core/src/main/java/com/alatka/connection/core/support/MessageGroupProcessorHandler.java
+++ alatka-connection-core/src/main/java/com/alatka/connection/core/support/MessageGroupProcessorHandler.java
@@ -11,6 +11,13 @@ import java.util.Collection;
  */
 public interface MessageGroupProcessorHandler<T, S> extends CustomMessageHandler<Collection<Message<T>>, S> {
 
+    S processMessageGroup(Collection<Message<T>> messages);
+
+    @Override
+    default S doExecute(Message<Collection<Message<T>>> message) {
+        return processMessageGroup(message.getPayload());
+    }
+
     @Override
     default boolean returnRawType() {
         return true;
